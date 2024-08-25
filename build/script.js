d37265e464336014a606ea78c4eface9f80f479c
Markme44cvsu <markjohn.ignacio@cvsu.edu.ph>
2024-08-25 21:09:36 +0800

    Update script.js

--- build/script.js
+++ build/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', async function() {
     let counterElement = document.getElementById('visitor-counter');
     try {
-        let response = await fetch("https://bt58yyqlxc.execute-api.us-east-1.amazonaws.com/prod/views");
+        let response = await fetch("https://7kjzfvb11h.execute-api.us-east-1.amazonaws.com/prod/views");
         let data = await response.json();
         counterElement.innerHTML = `Visitors: ${data.views}`;
     } catch (error) {
