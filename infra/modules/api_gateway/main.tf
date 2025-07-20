06f353ba86ac6da213c94d97d710634923093a25
Markme44cvsu <markjohn.ignacio@cvsu.edu.ph>
2025-07-20 15:36:52 +0800

    Fix API Gateway deployment configuration and add terraform destroy workflow

--- infra/modules/api_gateway/main.tf
+++ infra/modules/api_gateway/main.tf
@@ -42,7 +42,12 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   ]
 
   rest_api_id = aws_api_gateway_rest_api.cloud_resume_api.id
-  stage_name  = "prod"
+}
+
+resource "aws_api_gateway_stage" "prod_stage" {
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.cloud_resume_api.id
+  stage_name    = "prod"
 }
 
 resource "aws_api_gateway_method" "options_method" {
@@ -116,5 +121,5 @@ resource "aws_api_gateway_integration_response" "post_integration_response" {
 }
 
 output "api_url" {
-  value = "${aws_api_gateway_deployment.api_deployment.invoke_url}/views"
+  value = "${aws_api_gateway_stage.prod_stage.invoke_url}/views"
 }
